e14a22e9dabd3ca5f6db01c992128ccc12dd9739
Toshiyuki HINA <thina@yourinventit.com>
2019-02-04 11:53:36 +0900

    initial commit

---
+++ lib/jquery/elycharts/rails.rb
@@ -0,0 +1,10 @@
+require "jquery/elycharts/rails/version"
+
+module Jquery
+  module Elycharts
+    module Rails
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/jquery/elycharts/rails_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Jquery::Elycharts::Rails do
+  it "has a version number" do
+    expect(Jquery::Elycharts::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
